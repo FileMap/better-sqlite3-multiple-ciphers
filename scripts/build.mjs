9c954ac7171ce59121592ac548950549fe9ce9dc
Ozan Akın <ozn.akn@gmail.com>
2022-12-23 22:36:29 +0300

    do not use napi builds

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,9 +2,9 @@
 
 const archs = process.platform === 'win32' ? ['x64'] : ['x64', 'arm64'];
 
-const nodeVersions = ['16.0.0'];
-const electronVersions = ['20.0.0'];
+const nodeVersions = ['16.0.0', '17.0.1', '18.0.0'];
+const electronVersions = ['19.0.0', '20.0.0', '21.0.0', '22.0.0'];
 
 for (const arch of archs) {
-    await $`npx prebuildify --strip --napi --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
+    await $`npx prebuildify --strip --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
 }
