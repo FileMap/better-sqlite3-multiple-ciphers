60e0849cfb4d7e49a2c9a2e215225d6edc7b1f3f
Ozan Akın <ozn.akn@gmail.com>
2022-12-23 17:33:00 +0300

    downgrade electron version

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,7 +3,7 @@
 const archs = process.platform === 'win32' ? ['x64'] : ['x64', 'arm64'];
 
 const nodeVersions = ['16.0.0'];
-const electronVersions = ['21.0.0'];
+const electronVersions = ['20.0.0'];
 
 for (const arch of archs) {
     await $`npx prebuildify --strip --napi --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
